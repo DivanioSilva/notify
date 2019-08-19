9fb357beda5f129bc43c13cb699a99ef7c897dd6
Divanio Silva <dcsilva@mail.ru>
2019-08-19 12:49:04 +0100

    Add service layer

---
+++ src/app/events.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { EventResponse, Achnowledgmenet} from './interfaces';
+import { HttpClient} from '@angular/common/http';
+import { Observable, Observer, onErrorResumeNext} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsService {
+  private endpoint = 'https://us-central1-ps-notify-api.cloudfunctions.net/api';
+
+  constructor(private http: HttpClient){}
+
+
+  getAll(): Observable<EventResponse>{
+    return Observable.create((observer: Observer<EventResponse>) => {
+      const self = this;
+      self.getLatest().subscribe( res => onNext(res), observer.error);
+
+      funcion onNext(response: EventResponse){
+        observer.next(response);
+        if(response.links.next){
+          self.getByRouter<EventResponse>(response.links.next).subscribe(res => onNext(res), observer.error);
+        } else{
+          observer.complete();
+        }
+
+      }
+    });
+  }
+
+  private getByRouter<T>(router: String): Observable<T>{
+      const url = '${this.endpoint}${route}';
+      return this.http.get<T>;
+  }
+
+  getLatest(): Observable<EventResponse>{
+      const route = '/latest';
+      return this.getByRouter(route);
+  }
+
+  getById(id: number): Observable<EventResponse>{
+      const route = '/event/${id}';
+      return this.getByRouter(route);
+  }
+
+  getAcknowledgements(event: EventResponse): Observable<Achnowledgmenet[]>{
+      return this.getByRouter<Achnowledgmenet[]>(event.links.acknowledgements);
+  }
+}
---
+++ src/app/interfaces.ts
@@ -0,0 +1,41 @@
+import { Address } from 'cluster';
+
+export interface EventResponse{
+    event: EmergencyEvent;
+    links: LinkStyle;
+}
+
+export interface EmergencyEvent{
+    id: number;
+    created: Date;
+    caller: Caller;
+    dialed: String;
+}
+
+export interface Caller{
+    id: String;
+    firstName: String;
+    lastName: String;
+    phone: String;
+    address: Address;
+}
+
+export interface Address{
+    street1: String;
+    street2: String;
+    city: String;
+    state: String;
+    zip: String;
+}
+
+export interface Achnowledgmenet{
+    timestamp: Date;
+    user: String;
+    note: String;
+}
+
+export interface Links{
+    self: String;
+    next: String;
+    acknowledgements: String;
+}
